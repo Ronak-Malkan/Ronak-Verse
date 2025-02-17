ddb1a25c8d3187c51a0d5f10229bb9b359ec757d
RonakMalkan <ronakmalkan2062001@gmail.com>
2025-02-17 00:04:10 -0800

    init.sh gives permissions to all the files to run on droplet

--- init.sh
+++ init.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+
+chmod +x init.sh nginx.sh services/Gateway/deploy.sh services/TwoCars/deploy.sh services/TypeItToLoseIt/deploy.sh
+
 echo "Starting initial configuration..."
 ./basic-config.sh
 ./services/Gateway/deploy.sh
