4b0c1cebb206469bdf5789ea1e9817ea946f007d
RonakMalkan <ronakmalkan2062001@gmail.com>
2025-04-15 19:12:32 -0700

    made Portfolio/deploy.sh executable

--- init.sh
+++ init.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-chmod +x init.sh nginx.sh services/Gateway/deploy.sh services/TwoCars/deploy.sh services/TypeItToLoseIt/deploy.sh
+chmod +x init.sh nginx.sh services/Gateway/deploy.sh services/TwoCars/deploy.sh services/TypeItToLoseIt/deploy.sh services/Portfolio/deploy.sh basic-config.sh
 
 echo "Starting initial configuration..."
 ./basic-config.sh
