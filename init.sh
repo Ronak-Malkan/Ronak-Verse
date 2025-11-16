9d9abf1ccf93e425249a747de4afd74dbbf4fa69
RonakMalkan <ronakmalkan2062001@gmail.com>
2025-11-15 19:59:58 -0800

    added Windborne/deploy.sh execution in init.sh

--- init.sh
+++ init.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-chmod +x init.sh nginx.sh services/Gateway/deploy.sh services/TwoCars/deploy.sh services/TypeItToLoseIt/deploy.sh services/Portfolio/deploy.sh basic-config.sh
+chmod +x init.sh nginx.sh services/Gateway/deploy.sh services/TwoCars/deploy.sh services/TypeItToLoseIt/deploy.sh services/Portfolio/deploy.sh services/WindBorne/deploy.sh basic-config.sh
 
 echo "Starting initial configuration..."
 ./basic-config.sh
@@ -8,6 +8,8 @@ echo "Starting initial configuration..."
 ./services/TwoCars/deploy.sh
 ./services/TypeItToLoseIt/deploy.sh
 ./services/Portfolio/deploy.sh
+./services/WindBorne/deploy.sh
+
 # ./database/deploy.sh
 ./nginx.sh
 echo "Configuration complete."
